ced936959b8198926fec197a55e870b342d548fb
Kaspar Sinitsin <kaspar.sinitsin@students.beaminstitute.org>
2025-02-11 15:00:32 +0100

    styling changes in Calendar

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -13,7 +13,7 @@ const MyCalendar = () => {
 
   return (
     <MainLayout>
-      <div className="h-screen flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-start pt-8">
         <h2 className="text-4xl font-bold text-green-800 mb-8">My Calendar</h2>
         <div className="shadow-lg rounded-lg">
           <Calendar
