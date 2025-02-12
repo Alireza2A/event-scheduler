d059c8bd932c09bf249875d5dc02457c78d9ea1a
ARDoagooei <a.r.doagooei@gmail.com>
2025-02-12 23:40:07 +0100

    Implementing calendar logic

--- src/data/events.js
+++ src/data/events.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "http://localhost:3001/api/events";
 import { BASE_URL } from "./EventsApiURL";
-
+import { getSelectedData } from "../data/localStorage";
 const getAllEvents = async () => {
     try {
         const res = await fetch(`${BASE_URL}/events`, {
@@ -13,7 +13,8 @@ const getAllEvents = async () => {
         if (res.ok) {
             const data = await res.json();
             console.log("data", data);
-            return data.results;
+            const allEvents = data.results;
+            return filterAllEventsWithSelectedDate(allEvents);
         }
     } catch (error) {
         console.error("Error occured while retriving all events:", error);
@@ -38,4 +39,12 @@ const getEventById = async (eventId) => {
 
     // return Array.from(allEvents).find((ev) => ev.id == eventId);
 };
+function filterAllEventsWithSelectedDate(allEvents) {
+    const selectedDate = getSelectedData();
+    if (selectedDate === "") {
+        return allEvents;
+    }
+
+    return allEvents.filter((ev) => new Date(ev.date).toDateString() == selectedDate);
+}
 export { getAllEvents, getEventById };
